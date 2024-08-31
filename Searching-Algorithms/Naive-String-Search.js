b911dff09f14aec27965503546083ec9ed2370ee
Ayoub-Mabrouk <ayoubmabroukcontact@gmail.com>
2024-08-31 14:15:58 +0100

    Searching algorithms

---
+++ Searching-Algorithms/Naive-String-Search.js
@@ -0,0 +1,12 @@
+function naiveStringSearch(text,searchingFor){
+    let counter = 0;
+    const textLen = text.length;
+    const wordLen = searchingFor.length;
+    for(let i = 0;i <= textLen - wordLen;i++){
+        for(let j = 0;j<wordLen;j++){
+            if(searchingFor[j] !== text[i+j]) break;
+            if(wordLen == j+1) counter++;
+        }
+    }
+    return counter;
+}
