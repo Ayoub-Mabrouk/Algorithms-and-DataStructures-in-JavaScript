a98f28ba733fa9677de336a109e449dbc1c09f0c
Ayoub-Mabrouk <ayoubmabroukcontact@gmail.com>
2024-09-13 02:07:27 +0100

    add sorting algorithm merge sort

---
+++ Sorting-Algorithms/Merge-Sort.js
@@ -0,0 +1,16 @@
+function mergeSort(array) {
+   if (array.length <= 1) return array;
+   const middle = Math.floor(array.length / 2);
+   return merge(mergeSort(array.slice(0, middle)),mergeSort(array.slice(middle)));
+}
+
+function merge(left, right) {
+   let result = [], i = 0, j = 0;
+   while (i < left.length && j < right.length) {
+       result.push(left[i] <= right[j] ? left[i++] : right[j++]);
+   }
+   return result.concat(left.slice(i), right.slice(j));
+}
+
+let generatedArray = Array.from({ length:10 }, () => Math.floor(Math.random() * (10 - 0 + 1)) + 0);
+console.log(mergeSort(generatedArray));
